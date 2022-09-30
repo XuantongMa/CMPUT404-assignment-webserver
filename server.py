5edfa1e97027d6746d3420295b8f66fb98f0a454
XuantongMa <xuantong@ualberta.ca>
2022-09-30 02:32:00 -0600

    code version1

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os.path
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -29,18 +30,85 @@ import socketserver
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
+    def css_file(self, path):
+        file = open( "./www"+ path,'r').read()
+        status_code = "HTTP/1.1 200 OK\r\n"
+        content_type = "Content-Type: text/css;\r\n"
+        length = str(len(file))
+        content_length = "Content-Length: {}\r\n".format(length)
+        connection = "Connection : close\r\n"
+        respond = status_code + content_type + content_length  + connection + file
+        #print("111111111111111111111111111111111111111111")
+        return respond
+    
+    def html_file(self, path):
+        file = open( "./www"+ path,'r').read()
+        status_code = "HTTP/1.1 200 OK\r\n"
+        content_type = "Content-Type: text/html;\r\n"
+        length = str(len(file))
+        content_length = "Content-Length: {}\r\n".format(length)
+        connection = "Connection : close \r\n"
+        respond = status_code + content_type + content_length + connection + file
+        #print("222222222222222222222222222222222222222222")
+        return respond
+
+    def index_file1(self, path):
+        path = f"./www{path}index.html"
+        if open( path,'r').read():
+            file = open( path,'r').read()
+            status_code = "HTTP/1.1 200 OK\r\n"
+            content_type = "Content-Type: text/html;\r\n"
+            length = str(len(file))
+            content_length = ("Content-Length: {}\r\n".format(length))
+            connection = "Connection : close \r\n"
+            respond = status_code + content_type + content_length + connection + file
+            #print("333333333333333333333333333333333333333333")                         
+        else:
+            respond = "HTTP/1.1 404 Not Found"
+            #print("4444444444444444444444444444444444444444")
+        return respond
+
+    def index_file2(self, path):
+        if os.path.isfile("./www{}/index.html".format(path)):
+            status_code = "HTTP/1.1 301 Moved Permanently"
+            location= "Location: http://127.0.0.1:8080{}/".format(path)
+            respond = "{0}\r\n{1}\r\n{2}".format(status_code, location, "\r\n")
+            #print("55555555555555555555555555555555555555")
+        else:
+            respond = "HTTP/1.1 404 Not Found"
+            #print("666666666666666666666666666666666666666666")
+        return respond
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        request = self.data.decode().split()
+        method = request[0]
+        path = request[1]
+        if method != "GET":
+            respond = "HTTP/1.1 405 Method Not Allowed"
+            #print("777777777777777777777777777777777777")
+        else:
+            check = os.path.realpath(f"./www/{path}").startswith(os.path.realpath("./www"))
+            if check == False:
+                respond = "HTTP/1.1 404 Not Found"
+                #print("88888888888888888888888888888888888")   
+            else:
+                if os.path.exists("./www{}".format(path)) and path.endswith("css"):
+                        respond = self.css_file(path)
+                elif os.path.exists("./www{}".format(path)) and path.endswith("html"):
+                        respond = self.html_file(path)                          
+                elif os.path.exists("./www{}/index.html".format(path)) and path.endswith("/"):
+                        respond = self.index_file1(path)
+                else:
+                        respond = self.index_file2(path)
+        self.request.sendall(respond.encode())
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
-
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
-
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
